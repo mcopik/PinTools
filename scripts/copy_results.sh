e255ad77b7e3a738525f6f81942859f6dab9956b
Marcin Copik <mcopik@gmail.com>
2019-02-20 20:55:48 +0100

    Make file name a script parameter

--- scripts/copy_results.sh
+++ scripts/copy_results.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-PIN_ROOT=$1
+TEST_SUITE_BUILD=$1
 SPEC_ROOT=$2
-OUT_DIR=$3
-file_name="inscount.out_branches"
+FILE_NAME=$3
+OUT_DIR=$4
+#file_name="inscount.out_branches"
 
 # Locate PIN analysis results in the SPEC
-for x in `find ${SPEC_ROOT} -name "${file_name}"`; do
+for x in `find ${SPEC_ROOT} -name "${FILE_NAME}"`; do
 x2=${x#"${SPEC_ROOT}/benchspec/CPU2006/"}
 x4=${x2%%/*}
 mv $x ${OUT_DIR}/${x4}_branches
 done
 
-for x in `find ${PIN_ROOT} -name "${file_name}"`; do
-x2=${x#"${PIN_ROOT}/External/SPEC/"}
+for x in `find ${TEST_SUITE_BUILD} -name "${FILE_NAME}"`; do
+x2=${x#"${TEST_SUITE_BUILD}/External/SPEC/"}
 x2=${x2#*/}
 x2=${x2%%/*}
 mv ${x} ${OUT_DIR}/${x2}_branches
